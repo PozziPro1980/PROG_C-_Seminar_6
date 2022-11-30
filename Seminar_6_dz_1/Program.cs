069dc9a28c69573b9c7c159ab28ed0f88fd8b35b
Yuri <zver22221980@gmail.com>
2022-11-30 23:14:11 +0700

    ДЗ№ 1 семинар_6.

--- Seminar_6_dz_1/Program.cs
+++ Seminar_6_dz_1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Write("Сколько чисел ввести? ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [num];
+
+int PositiveNumber(int count)
+{
+    int countPositive = 0;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        if (array[i] > 0)
+        {
+            countPositive+=1;
+        }
+        Console.WriteLine(string.Join("," ,array));
+    }
+    return countPositive;
+}
+int newCount = PositiveNumber(num);
+Console.Write($"Количество положительных чисел = {newCount}");
